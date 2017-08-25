2aee3c9db24f6a0fdde9fa5f4fca968fa73f2f45
shaoqiu <shaoqiu@shaoqiudeiMac.local>
2017-08-25 09:40:10 +0800

    Initial Commit

---
+++ 20170825_OpenGLESDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20170825_OpenGLESDemo
+//
+//  Created by shaoqiu on 2017/8/25.
+//  Copyright © 2017年 shaoqiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20170825_OpenGLESDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20170825_OpenGLESDemo
+//
+//  Created by shaoqiu on 2017/8/25.
+//  Copyright © 2017年 shaoqiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
